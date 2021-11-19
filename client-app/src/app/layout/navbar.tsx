b44b0d8e7f0635e3041195161d3e5743f1d3bc1c
cuibasw01 <cuibasw01@CUID00466>
2021-11-19 20:09:06 +0100

    section 5

--- client-app/src/app/layout/navbar.tsx
+++ client-app/src/app/layout/navbar.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Button, Container, Menu } from "semantic-ui-react";
-import logo from "/logo192.png";
 
-export default function NavBar() {
+interface Props {
+  openForm: () => void;
+}
+
+export default function NavBar({ openForm }: Props) {
   return (
     <Menu inverted fixed="top">
       <Container>
         <Menu.Item header>
-            {/* react knows that we look in public folder src */}
+          {/* react knows that we look in public folder src */}
           <img
             src="/assets/logo.png"
             alt="logo"
@@ -17,7 +20,7 @@ export default function NavBar() {
         </Menu.Item>
         <Menu.Item name="Activities" />
         <Menu.Item>
-          <Button positive content="Create Activity" />
+          <Button onClick={openForm} positive content="Create Activity" />
         </Menu.Item>
       </Container>
     </Menu>
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { act } from "react-dom/test-utils";
-import { Button, Card, Icon, Image } from "semantic-ui-react";
+import { Button, Card, Image } from "semantic-ui-react";
 import { Activity } from "../../../app/models/Activity";
 
 interface Props {
   activity: Activity;
+  cancelSelectActivity: () => void;
+  openForm: (id: string) => void;
 }
 
-export default function ActivityDetails({ activity }: Props) {
+export default function ActivityDetails({
+  activity,
+  cancelSelectActivity,
+  openForm,
+}: Props) {
   return (
     <Card>
-      <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+      <Image src={`assets/categoryImages/${activity.category}.jpg`} />
       <Card.Content>
         <Card.Header>{activity.title}</Card.Header>
         <Card.Meta>
@@ -20,8 +25,18 @@ export default function ActivityDetails({ activity }: Props) {
       </Card.Content>
       <Card.Content extra>
         <Button.Group widths="2">
-          <Button basic color="blue" content="Edit" />
-          <Button basic color="grey" content="Cancel" />
+          <Button
+            onClick={() => openForm(activity.id)}
+            basic
+            color="blue"
+            content="Edit"
+          />
+          <Button
+            onClick={cancelSelectActivity}
+            basic
+            color="grey"
+            content="Cancel"
+          />
         </Button.Group>
       </Card.Content>
     </Card>
